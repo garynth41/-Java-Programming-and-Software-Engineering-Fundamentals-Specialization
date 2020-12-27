7cfa69f9f145e765ae33918c5379976c4d4f5da7
Gary N. Thomas <45762811+garynth41@users.noreply.github.com>
2020-12-27 15:19:55 -0500

    Delete script.js

--- Programming Foundations with JavaScript, HTML and CSS/Week 3/Event Driven Programming/Image Upload/script.js
+++
@@ -1,7 +0,0 @@
-function upload() {
-	var uploadInput = document.getElementById("btnUpload");
-	var image = new SimpleImage(uploadInput);
-	var canvas = document.getElementById("can");
-	
-	image.drawTo(canvas);
-}
